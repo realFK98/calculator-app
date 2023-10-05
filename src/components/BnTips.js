e36da8201a4b4613db8121f1e812daf1e20b7b25
realFK98 <wishira2013.coth@gmail.com>
2023-10-05 23:59:05 +0700

    f

--- src/components/BnTips.js
+++ src/components/BnTips.js
@@ -2,7 +2,7 @@ export default function BnTips({percent, onHandelChange}) {
   return (
     <button
       data={percent}
-      className={`bn-tip text-[#F4FAFA]  bg-[#00494D] w-[145px] mb-3  rounded-[8px] h-14 `}
+      className={`bn-tip text-[#F4FAFA]  bg-[#00494D] w-[135px] mb-3  rounded-[8px] h-14 `}
       onClick={onHandelChange}
     >
       {percent}%
